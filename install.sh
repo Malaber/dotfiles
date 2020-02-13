466df1022928cd6e1806ef2db94e99edc17dbef5
Malaber <github@schaedler.rocks>
2020-02-13 10:18:19 +0100

    Add hint to arch package compression and build speed

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 cp .tmux/.tmux.conf.local .
 
+echo  "Read https://bbs.archlinux.org/viewtopic.php?id=127894 to disable compression and speed up package building"
+
 sudo chsh -s /bin/zsh $USER
 
 ./dotfiles/apply.sh
