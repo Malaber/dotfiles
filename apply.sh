9d497ae688010b0d469bff9799015b3b148aa406
Malaber <git@schaedler.rocks>
2022-07-18 15:13:46 +0200

    Remove arch specific stuff from apply.sh

--- apply.sh
+++ apply.sh
@@ -4,22 +4,18 @@ SCRIPTPATH=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPTPATH")
 #echo $BASEDIR
 
-sudo pacman -S --needed $(cat $BASEDIR/packages_slim)
-yay -S --needed $(cat $BASEDIR/packages)
-
 ln -sf $BASEDIR/.bashrc ~/
 ln -sf $BASEDIR/.profile ~/
 ln -sf $BASEDIR/.aliases ~/
 ln -sf $BASEDIR/.tmux.conf.local ~/
 ln -sf $BASEDIR/.zshrc ~/
-ln -sf $BASEDIR/.config/plasma-workspace/env/askpass.sh ~/.config/plasma-workspace/env/askpass.sh
 
 git config --global core.excludesfile "$BASEDIR/.global_gitignore"
 git config --global user.useConfigOnly true
 git config --global alias.pushall '!git remote | xargs -L1 git push'
 git config --global alias.oldest-ancestor '!bash -c '\''diff --old-line-format='' --new-line-format='' <(git rev-list --first-parent "${1:-master}") <(git rev-list --first-parent "${2:-HEAD}") | head -1'\'' -' # https://stackoverflow.com/a/4991675/10559526
 
-sudo cp -f $BASEDIR/dschaedler.zsh-theme /usr/share/oh-my-zsh/themes/dschaedler.zsh-theme
+cp -f $BASEDIR/dschaedler.zsh-theme /Users/dschaedler/.antigen/bundles/robbyrussell/oh-my-zsh/themes/dschaedler.zsh-theme
 
 echo -e "\nAPPLY\n" >> $BASEDIR/LastRun
 date >> $BASEDIR/LastRun
