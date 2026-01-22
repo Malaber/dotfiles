59c5b4e2a1bb0063dc1893805a18bc1bd923e64e
Malaber <git@schaedler.rocks>
2026-01-22 09:24:49 +0100

    Reorder git config

--- run_10_install.sh
+++ run_10_install.sh
@@ -4,14 +4,22 @@ UNDERCOVER_FILE="$HOME/.topsecretfbiundercover"
 touch $UNDERCOVER_FILE
 chmod 600 $UNDERCOVER_FILE
 
+# git
 git config --global core.excludesfile "$HOME/.global_gitignore"
 git config --global user.useConfigOnly true
-git config --global alias.pushall '!git remote | xargs -L1 git push'
-git config --global alias.oldest-ancestor '!bash -c '\''diff --old-line-format='' --new-line-format='' <(git rev-list --first-parent "${1:-master}") <(git rev-list --first-parent "${2:-HEAD}") | head -1'\'' -' # https://stackoverflow.com/a/4991675/10559526
 git config --global push.autoSetupRemote true
-git config --global alias.pushfwl "push --force-with-lease"
 git config --global gpg.format "ssh"
 
-# force lf everywhere
+## force user config to be per repository
+git config --global --unset user.name
+git config --global --unset user.email
+git config --global --unset user.signingkey
+
+## force lf everywhere
 git config --global core.autocrlf false
 git config --global core.eol lf
+
+## aliases
+git config --global alias.pushall '!git remote | xargs -L1 git push'
+git config --global alias.oldest-ancestor '!bash -c '\''diff --old-line-format='' --new-line-format='' <(git rev-list --first-parent "${1:-master}") <(git rev-list --first-parent "${2:-HEAD}") | head -1'\'' -' # https://stackoverflow.com/a/4991675/10559526
+git config --global alias.pushfwl "push --force-with-lease"
